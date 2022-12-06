412cca4b714d7faec3cacc8895fb638929ce76fb
ndikumanaisaie <54780027+ndikumanaisaie@users.noreply.github.com>
2022-12-06 23:47:00 +0100

    Create table animal

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE Animals (
+	id INT PRIMARY KEY NOT NULL,
+	name TEXT,
+	Date_of_birth DATE,
+	escape_attempts INT,
+	neutered BIT,
+	weight_kg REAL
 );
