2e9ea162370ea2b90e89b6b71fc6586b37baffad
ndikumanaisaie <54780027+ndikumanaisaie@users.noreply.github.com>
2022-12-07 14:37:23 +0100

    Execute transactions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,26 @@ SELECT name, escape_attempts FROM Animals WHERE weight_kg>10.5;
 SELECT name FROM Animals WHERE neutered IS TRUE;
 SELECT name FROM Animals WHERE name NOT IN ('Gabumon');
 SELECT name FROM Animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth>'2022-01-01';
+SAVEPOINT birth;
+UPDATE animals SET weight_kg=-1*weight_kg;
+ROLLBACK TO birth;
+UPDATE animals SET weight_kg=-1*weight_kg WHERE weight_kg<0;
+SELECT * FROM animals;
+COMMIT; 
